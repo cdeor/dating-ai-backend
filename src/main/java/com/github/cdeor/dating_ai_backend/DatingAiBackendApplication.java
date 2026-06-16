ad29a7166b6e95027d5662bb34f723d47ed892b6
Shubh Pandey <42703943+cdeor@users.noreply.github.com>
2026-06-16 14:55:05 +0530

    init commit

---
+++ src/main/java/com/github/cdeor/dating_ai_backend/DatingAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.cdeor.dating_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cdeor/dating_ai_backend/DatingAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cdeor.dating_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
